0924af8102cd0f9aaf2329408b66da18908b628d
Crown 4 one <ratul@Ratuls-MacBook-Pro.local>
2023-05-24 14:03:13 +0530

    Updates

---
+++ precache-manifest.277612d3113540cba6223aeba2f10b9f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48f9bb043a4e0a586ea0011b28b18c7",
+    "url": "/Notes-Keeper-App/index.html"
+  },
+  {
+    "revision": "28590cc89be37effedf3",
+    "url": "/Notes-Keeper-App/static/js/2.e191c4e3.chunk.js"
+  },
+  {
+    "revision": "385c80333c7324af1114",
+    "url": "/Notes-Keeper-App/static/js/main.e64bfe60.chunk.js"
+  },
+  {
+    "revision": "dd25312b5215b2186db2",
+    "url": "/Notes-Keeper-App/static/js/runtime-main.81be8131.js"
+  }
+]);
